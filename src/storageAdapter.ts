e5424e2bccab1bb33f8020c7379d28eb6d3ed9fa
chengbotao <chengbotao5221@163.com>
2024-01-03 00:17:45 +0800

    feat: 🌟 代码结构&基础逻辑完成

--- src/storageAdapter.ts
+++ src/storageAdapter.ts
@@ -2,23 +2,63 @@
  * @Author: Chengbotao
  * @Contact: https://github.com/chengbotao
  */
+
+import { typeOperations } from "./typeOperationsMap";
+
+// https://developer.mozilla.org/zh-CN/docs/Web/API/Storage
+
 enum StorageType {
 	Local = "local",
 	Session = "session",
 }
+
 export abstract class WebStorage {
 	private storage: Storage;
 	constructor(storageType: StorageType) {
 		this.storage =
 			storageType === StorageType.Local ? localStorage : sessionStorage;
 	}
-    get length() {
-        return this.storage.length;
-    }
-	clear() {
-		this.storage.clear();
+	get(key: string) {
+		const data = JSON.parse(this.storage.getItem(key)!);
+		if (data) {
+			const {type, value} = data;
+			if (typeOperations.has(type)) {
+				return typeOperations.get(type)!.deserialize(value);
+			} else {
+				return value;
+			}
+		} else {
+			return data;
+		}
 	}
-	key(index: number) {
-		return this.storage.key(index);
+	set(key: string, value: unknown) {
+		const type = Object.prototype.toString.call(value).replace(/^\[object (.+)\]$/, "$1");
+		if (typeOperations.has(type)) {
+			value = typeOperations.get(type)!.serialize(value);
+		} else {
+			value = JSON.stringify({type,value});
+		}
+		this.storage.setItem(key, value as string);
+	}
+	remove(key: string) {
+		this.storage.removeItem(key);
+	}
+	get length() {
+		return this.storage.length;
+	}
+	get clear() {
+		return this.storage.clear;
+	}
+	get key() {
+		return this.storage.key;
+	}
+	get getItem() {
+		return this.get;
+	}
+	get setItem() {
+		return this.set;
+	}
+	get removeItem() {
+		return this.remove;
 	}
 }
